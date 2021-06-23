edd992c01aa6638e285ddb93dd128ecafb3959e5
xugd123 <979104293@qq.com>
2021-06-23 19:05:01 +0800

    C1客户端

--- src/main/java/com/example/redisdemo/RedisdemoApplication.java
+++ src/main/java/com/example/redisdemo/RedisdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedisdemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("c1客户端");
         SpringApplication.run(RedisdemoApplication.class, args);
     }
 
